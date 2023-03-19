1d02894fa957f6a4a0ebebe49115e2b838ebe61d
LeanBorquez <leandroborqueza@gmail.com>
2023-03-19 10:24:51 -0300

    Voting-Config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
-require("@nomicfoundation/hardhat-toolbox");
-
+//require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      },
+    },
+  },
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
